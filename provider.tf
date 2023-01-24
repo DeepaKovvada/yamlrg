dce17c15b364fa50aab6fa94e2e87bfef0513456
DeepaKovvada <118762067+DeepaKovvada@users.noreply.github.com>
2023-01-24 17:49:30 -0500

    Add files via upload

---
+++ provider.tf
@@ -0,0 +1,8 @@
+provider "azurerm" {
+  features {}
+
+  subscription_id = "9e04de8c-d344-4b9d-9da1-606e614944df"
+  client_id       = "35b0341a-cbf8-4e79-ad34-e9b98bc2d7db"
+  client_secret   = "LHM8Q~ipunK2HaBWgbBwTVAd-wwOAWhjCCsg6b5T"
+  tenant_id       = "b4edbf0a-eecd-4b80-8486-28231a271593"
+}
